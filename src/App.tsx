2f0cac94dde3f329d2fe9c201abea637dd412c3a
kometa kosmos <xxkometa@gmail.com>
2023-03-30 06:50:27 +0300

    remove unused code from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { fetchSlider } from './API/request';
 import './App.css';
 import Layout from './components/Layout/Layout';
 import About from './pages/About/About';
@@ -10,9 +8,6 @@ import BlogDetailsPage from './pages/BlogPage/BlogDetails';
 import ContactsPage from './pages/ContactsPage/ContactsPage';
 import Main from './pages/Main/Main';
 import ServicesPage from './pages/Services/ServicesPage';
-import ServicesMore from './pages/Services_detail/Services_Detail';
-
-const data = fetchSlider().then((item) => item);
 
 function App() {
   return (
